b24336480410541ba6d801e44c9bf51beef2dee8
flamekaiser007 <arpitmaurya262005@gmail.com>
2026-04-10 02:06:56 +0530

    update API URL to new render URL

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Footer from './Components/footer'
 import Transaction from './Components/transaction'
 import Login from './Components/Login'
 import Signup from './Components/Signup'
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
